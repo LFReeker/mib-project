8674157fe715e498b59e6eb29d186fccd0f37d2f
Lars Reeker <larsreeker@gmail.com>
2015-04-16 17:57:51 -0400

    Added location matrix and cosine similarity

--- Step1_preprocessing.R
+++ Step1_preprocessing.R
@@ -126,7 +126,168 @@ nrow(call.data.grouped)
 sunday_grouped <- ddply(call.data, c("caller_id", "callee_id"), summarize, weight=(length(callee_id)*sum(call_duration)))
 write.csv(sunday_grouped, "sunday_freq.csv")
 
+### Compute cosine similarity
+############
+call.data <- read.csv("call_data_sunday_32908obs.csv", header = TRUE)
 
+library(plyr)
+freq <- ddply(call.data, "caller_id", summarize, freq=length(caller_id))
+
+user1 <- 20460812053 # 10
+user2 <- 17614317538 # 6
+
+rec_towers <- unique(call.data$cell_id) #2568 cell towers recorded
+
+users <- unique(call.data$caller_id) # retrieve all unique callers (callees do not have location)
+
+
+
+for(user in call.data) {
+  index <- call.data$caller_id %in% user1 # create a logical vector of all instances with caller_id = user
+  list <- call.data[index, ] # list all connections of user
+  list
+  
+  list_time <- list$time # list all times a connection is made
+  list_time
+  data.frame(date=list$time, time=format(x, "%H:%M"))
+  
+  
+  
+  
+  list_tower <- list$cell_id # retrieve list with all cell tower id's of user
+  index_tower <- rec_towers %in% list_tower # create logical vector with all visited cell towers
+  index_tower
+  
+  
+
+  
+
+detect_hour <- function(list_time) {  
+  output <- data.frame(index = numeric(length(list_time)), times = factor(length(list_time)), hour = numeric(length(list_time)))
+  output$times <- factor(list_time)
+  
+  x0 <- as.POSIXct("2010-12-01 00:00:00", tz = "GMT")
+  x1 <- as.POSIXct("2010-12-01 01:00:00", tz = "GMT")
+  x2 <- as.POSIXct("2010-12-01 02:00:00", tz = "GMT")
+  x3 <- as.POSIXct("2010-12-01 03:00:00", tz = "GMT")
+  x4 <- as.POSIXct("2010-12-01 04:00:00", tz = "GMT")
+  x5 <- as.POSIXct("2010-12-01 05:00:00", tz = "GMT")
+  x6 <- as.POSIXct("2010-12-01 06:00:00", tz = "GMT")
+  x7 <- as.POSIXct("2010-12-01 07:00:00", tz = "GMT")
+  x8 <- as.POSIXct("2010-12-01 08:00:00", tz = "GMT")
+  x9 <- as.POSIXct("2010-12-01 09:00:00", tz = "GMT")
+  x10 <- as.POSIXct("2010-12-01 10:00:00", tz = "GMT")
+  x11 <- as.POSIXct("2010-12-01 11:00:00", tz = "GMT")
+  x12 <- as.POSIXct("2010-12-01 12:00:00", tz = "GMT")
+  x13 <- as.POSIXct("2010-12-01 13:00:00", tz = "GMT")
+  x14 <- as.POSIXct("2010-12-01 14:00:00", tz = "GMT")
+  x15 <- as.POSIXct("2010-12-01 15:00:00", tz = "GMT")
+  x16 <- as.POSIXct("2010-12-01 16:00:00", tz = "GMT")
+  x17 <- as.POSIXct("2010-12-01 17:00:00", tz = "GMT")
+  x18 <- as.POSIXct("2010-12-01 18:00:00", tz = "GMT")
+  x19 <- as.POSIXct("2010-12-01 19:00:00", tz = "GMT")
+  x20 <- as.POSIXct("2010-12-01 20:00:00", tz = "GMT")
+  x21 <- as.POSIXct("2010-12-01 21:00:00", tz = "GMT")
+  x22 <- as.POSIXct("2010-12-01 22:00:00", tz = "GMT")
+  x23 <- as.POSIXct("2010-12-01 23:00:00", tz = "GMT")
+  
+  for(i in 1:length(list_time)) { 
+      print(i)
+      y <- as.POSIXct(paste("2010-12-01 ",as.character(list_time[i])), tz = "GMT")
+      print(y)
+      print(as.character(list_time[i]))
+      
+      if(y > x0 & y < x1) {
+        hour <- 1
+      } else if(y > x1 & y < x2) {
+        hour <- 2
+      } else if(y > x2 & y < x3) {
+        hour <- 3
+      } else if(y > x3 & y < x4) {
+        hour <- 4
+      } else if(y > x4 & y < x5) {
+        hour <- 5
+      } else if(y > x5 & y < x6) {
+        hour <- 6
+      } else if(y > x6 & y < x7) {
+        hour <- 7
+      } else if(y > x7 & y < x8) {
+        hour <- 8
+      } else if(y > x8 & y < x9) {
+        hour <- 9
+      } else if(y > x9 & y < x10) {
+        hour <- 10
+      } else if(y > x10 & y < x11) {
+        hour <- 11
+      } else if(y > x11 & y < x12) {
+        hour <- 12
+      } else if(y > x12 & y < x13) {
+        hour <- 13
+      } else if(y > x13 & y < x14) {
+        hour <- 14
+      } else if(y > x14 & y < x15) {
+        hour <- 15
+      } else if(y > x15 & y < x16) {
+        hour <- 16
+      } else if(y > x16 & y < x17) {
+        hour <- 17
+      } else if(y > x17 & y < x18) {
+        hour <- 18
+      } else if(y > x18 & y < x19) {
+        hour <- 19
+      } else if(y > x19 & y < x20) {
+        hour <- 20
+      } else if(y > x20 & y < x21) {
+        hour <- 21
+      } else if(y > x21 & y < x22) {
+        hour <- 22
+      } else if(y > x22 & y < x23) {
+        hour <- 23
+      } else if(y > x23 & y < x0) {
+        hour <- 24
+      } else {
+        print("error!")
+      }
+      
+      output$index[i] <- i
+      output$times[i] <- list_time[i]
+      output$hour[i] <- hour
+     } # check in which hour the connection is made
+  return(output)
+}
+time_df <- detect_hour(list_time)
+time_df <- cbind(time_df, cell=list$cell_id)
+
+v_location <- rep(0, 300) # 300 = length(rec_towers)
+v_time <- list(list1=v_location, list2=v_location, list3=v_location, list4=v_location, 
+               list5=v_location, list6=v_location, list7=v_location, list8=v_location, 
+               list9=v_location, list10=v_location, list11=v_location, list12=v_location, 
+               list13=v_location, list14=v_location, list15=v_location, list16=v_location, 
+               list17=v_location, list18=v_location, list19=v_location, list20=v_location, 
+               list21=v_location, list22=v_location, list23=v_location, list24=v_location)
+
+time_freq <- ddply(time_df, c("hour", "cell"), summarize, freq=length(index))
+
+for(i in time_freq$hour) {
+  y <- paste("list", i, sep = "")
+  v_time[y] <- time_freq$freq
+}
+v_time
+
+
+
+# fake matrix creation
+user1 <- c(1,1,1,0,2,0,0,1,0)
+user2 <- c(1,0,1,0,1,0,0,1,0)
+matrix1 <- matrix(data = user1, nrow = 3, ncol = 3)
+matrix2 <- matrix(data = user2, nrow = 3, ncol = 3)
+
+# concatenate the matrices into vectors
+vector1 <- c(matrix1)
+vector2 <- c(matrix2)
+
+library(lsa) # package for cosine similarity
+cosine(vector1,vector2) # calculate cosine similarity between two vectors
 
 ## Close the connection
 mongo.disconnect(mongo)
